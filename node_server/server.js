796256bdccf33712d89547c6fa1b0fe18edddc71
hirarira <hirarira617@gmail.com>
2019-05-28 02:33:46 +0900

    ORマッパーのsequelizeを導入

--- node_server/server.js
+++ node_server/server.js
@@ -1,7 +1,32 @@
 "use strict";
 const Express = require("express");
 const BodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 const app = Express();
+
+const db = {
+  name: 'anime',
+  user: process.env.ANIME_DB_USER_NAME,
+  pass: process.env.ANIME_DB_PASS,
+  host: 'localhost',
+  dialect: 'mysql'
+};
+
+if(db.user == null || db.pass == null){
+  console.log("ENV:DB_UASENAME or DB_PASS is null");
+  return;
+}
+
+const sequelize = new Sequelize(db.name, db.user, db.pass, {
+  host: db.host,
+  dialect: db.dialect
+});
+
+// 接続確認用
+sequelize.authenticate()
+.then(()=>{
+  console.log("ok");
+});
 
 // urlencodedとjsonは別々に初期化する
 app.use(BodyParser.urlencoded({
