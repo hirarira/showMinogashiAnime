5721f415ef430fbd82e0c94ecdf5275729e11b0b
hirarira <hirarira617@gmail.com>
2019-05-31 02:29:34 +0900

    animeStoryのmodelを追加

---
+++ node_server/model/animeStory.js
@@ -0,0 +1,39 @@
+module.exports = class{
+  constructor(sequelize) {
+    const Sequelize = require('sequelize');
+    this.model = sequelize.define('animeStory', {
+      // フィールド名
+      id: {
+        // フィールドの型
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      tid: Sequelize.INTEGER,
+      count: Sequelize.INTEGER,
+      stTime: Sequelize.INTEGER,
+      edTime: Sequelize.INTEGER,
+      lastUpdate: Sequelize.INTEGER,
+      subTitle: Sequelize.TEXT,
+      minogashi: Sequelize.TINYINT,
+      comment: Sequelize.TEXT
+    }, {
+      // モデル名をそのままテーブル名として使う
+      freezeTableName: true,
+      timestamps: false
+    });
+  }
+  getAllAnimeStory(tid){
+    return this.model.findAll({
+      where: {
+        tid: tid
+      }
+    });
+  }
+  getAllMinogashiStory(){
+    return this.model.findAll({
+      where: {
+        minogashi: 0
+      }
+    });
+  }
+}
