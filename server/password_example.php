78bcdee471a72fcb16e4d399e6b7f9322a3e9624
hirarira <hirarira617@gmail.com>
2019-06-15 19:24:27 +0900

    PHPを使った旧方式のserverを削除

--- server/password_example.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $connect = array(
-    'user' => '****',
-    'pass' => '****'
-  );
-  $pdo = new PDO('mysql:dbname=anime;host=localhost;charset=utf8mb4', $connect['user'], $connect['pass']);
-?>
